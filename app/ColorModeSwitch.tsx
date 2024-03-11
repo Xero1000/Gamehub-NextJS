691ea2457d018869be74a34097b2eafa7b0d53b7
Xero1000 <alex.heil@comcast.net>
2024-03-11 08:56:56 -0400

    Build color mode switch

---
+++ app/ColorModeSwitch.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useColorMode, HStack, Switch, Text } from '@chakra-ui/react';
+import React from 'react'
+
+const ColorModeSwitch = () => {
+    const {toggleColorMode, colorMode} = useColorMode()
+
+  return (
+    <HStack>
+        <Switch colorScheme="green" isChecked={colorMode === "dark"} onChange={toggleColorMode}/>
+        <Text>Dark Mode</Text>
+    </HStack>
+  )
+}
+
+export default ColorModeSwitch
--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,15 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import Image from "next/image";
-import logo from "../public/logo.webp"
+import logo from "../public/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-        <Image src={logo} alt="logo" width={60} height={60}/>
-        <Text>NavBar</Text>
+    <HStack justify="space-between" padding="10px">
+      <Image src={logo} alt="logo" width={60} height={60} />
+      <ColorModeSwitch />
     </HStack>
-  )  
+  );
 };
 
 export default NavBar;
