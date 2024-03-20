0c945cb044360f54765b4f8c827ad04263d2b7b8
Xero1000 <alex.heil@comcast.net>
2024-03-20 10:02:29 -0400

    Refactoring- create a reusable API client

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -1,13 +1,29 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
     count: number;
     results: T[]
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: '5a00279a0cb347bcae546ecf4525c8a4'
     }
 })
+
+class APIClient<T> {
+    endpoint: string;
+
+    constructor(endpoint: string) {
+        this.endpoint = endpoint;
+    }
+
+    getAll = (config?: AxiosRequestConfig) => {
+        return axiosInstance
+            .get<FetchResponse<T>>(this.endpoint, config)
+            .then(res => res.data)
+    }
+}
+
+export default APIClient;
