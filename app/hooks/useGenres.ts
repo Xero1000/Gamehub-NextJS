00fcd83b40d8f5c9ef13e303e6c7c3ef948b6711
Xero1000 <alex.heil@comcast.net>
2024-03-24 07:24:45 -0400

    Implement infinite queries

--- app/hooks/useGenres.ts
+++ app/hooks/useGenres.ts
@@ -15,7 +15,7 @@ const useGenres = () =>
         queryKey: ['genres'],
         queryFn: apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000, // list of genres hardly ever changes so set this to 24 hours
-        initialData: { count: genres.length, results: genres}
+        initialData: { count: genres.length, results: genres }
 })
 
 export default useGenres;
