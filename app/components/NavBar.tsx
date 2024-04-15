9197dabf539599637678b36702353d58504d35c3
Xero1000 <alex.heil@comcast.net>
2024-04-15 08:18:18 -0400

    Add the login and logout links

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,18 +1,29 @@
+"use client";
+
 import { HStack } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { useSession } from "next-auth/react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <HStack padding="10px">
       <Link href="/">
-        <Image src={logo} alt="logo" width={90} height={90} objectFit="cover"/>
+        <Image src={logo} alt="logo" width={90} height={90} objectFit="cover" />
       </Link>
       <SearchInput />
       <ColorModeSwitch />
+      {status === "authenticated" && (
+        <Link href="/api/auth/signout">Logout</Link>
+      )}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </HStack>
   );
 };
--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -5,6 +5,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import theme from "../theme";
+import AuthProvider from "../auth/Provider";
 
 const queryClient = new QueryClient();
 
@@ -12,9 +13,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        {children}
-        <ReactQueryDevtools />
+        <AuthProvider>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          {children}
+          <ReactQueryDevtools />
+        </AuthProvider>
       </ChakraProvider>
     </QueryClientProvider>
   );
