35ee602513087cbea2673d60cc8c6e96b2f6eb51
Xero1000 <alex.heil@comcast.net>
2024-04-16 08:29:10 -0400

    Create wishlist page

---
+++ app/wishlist/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Box, Text } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+
+const WishlistPage = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return <Text>Loading...</Text>;
+
+  if (!session) {
+    return (
+      <Box>
+        <Text>You are not signed in</Text>
+        <Link href="api/auth/signin">Sign in</Link>
+      </Box>
+    );
+  }
+
+  return (
+    <Text>Wishlist Page</Text>
+  )
+};
+
+export default WishlistPage;
